abb660da441f413da802ce99533c1ea0de3b43bf
Pedro Lapa <pedro.lapa1911@gmail.com>
2025-05-20 21:19:40 -0300

    Merge com desenvolvimento

--- Acervo-api/target/generated-sources/annotations/com/github/petervl80/acervoapi/controller/mappers/ClientMapperImpl.java
+++ Acervo-api/target/generated-sources/annotations/com/github/petervl80/acervoapi/controller/mappers/ClientMapperImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-09T11:52:12-0300",
-    comments = "version: 1.6.0, compiler: javac, environment: Java 21.0.6 (Oracle Corporation)"
+    date = "2025-05-17T20:28:03-0300",
+    comments = "version: 1.6.0, compiler: javac, environment: Java 21.0.3 (Oracle Corporation)"
 )
 @Component
 public class ClientMapperImpl implements ClientMapper {
